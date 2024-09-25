0a0faee140667bbcffb7abf708f2972e1375de22
exfinen <47593166+exfinen@users.noreply.github.com>
2024-09-26 08:25:44 +0900

    wip

--- mpcstats/benchmark/gen_vms.py
+++ mpcstats/benchmark/gen_vms.py
@@ -15,7 +15,7 @@ for _, program, _, _ in all_protocols:
     print(f'Compiling {program}...')
 
     cmd = ['make', f'-j{os.cpu_count()}', program]
-    res = subprocess.run(cmd, cwd=repo_root, capture_output=True, text=True, env=env)
+    res = subprocess.run(cmd, cwd=repo_root, capture_output=True, text=True)
 
     if res.returncode != 0:
         print(f'--> Failed w/ return code {res.returncode}. {res.stderr}')
