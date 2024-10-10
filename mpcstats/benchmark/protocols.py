7ac800c19e048e1033293daf407eb41f17d607ec
exfinen <47593166+exfinen@users.noreply.github.com>
2024-10-10 17:08:03 +0900

    wip

--- mpcstats/benchmark/protocols.py
+++ mpcstats/benchmark/protocols.py
@@ -21,9 +21,9 @@ dishonest_majority_semi_honest = [t + ('DM-SH-SS',) for t in [
 
 honest_majority_malicious = [t + ('HM-M',) for t in [
 #('ring', 'replicated-ring-party.x', '--ring 128'), # ok
-('ps-rep-ring', 'ps-rep-ring-party.x', '--ring 128'), # ok
-('mal-rep-ring', 'malicious-rep-ring-party.x', '--ring 128'), # ok
-('sy-rep-ring', 'sy-rep-ring-party.x', '--ring 128'), # ok
+#('ps-rep-ring', 'ps-rep-ring-party.x', '--ring 128'), # ok
+#('mal-rep-ring', 'malicious-rep-ring-party.x', '--ring 128'), # ok
+#('sy-rep-ring', 'sy-rep-ring-party.x', '--ring 128'), # ok
 #('rep4-ring', 'rep4-ring-party.x', '--ring 128'), # ok, ssl, 4 parties
 ('ps-rep-field', 'ps-rep-field-party.x', ''), # ok
 ('sy-rep-field', 'sy-rep-field-party.x', ''), # ok
