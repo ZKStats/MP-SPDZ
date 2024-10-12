fba1f8434baeb2091631926631c3831196793a47
exfinen <47593166+exfinen@users.noreply.github.com>
2024-10-13 07:02:06 +0900

    wip

--- mpcstats/benchmark/protocols.py
+++ mpcstats/benchmark/protocols.py
@@ -1,14 +1,14 @@
 dishonest_majority_malicious = [t + ('DM-M-SS',) for t in [
-# ('mascot', 'mascot-party.x', ''), # ok
-# ('mama', 'mama-party.x', ''), # ok
+('mascot', 'mascot-party.x', ''), # ok
+('mama', 'mama-party.x', ''), # ok
 # ('spdz2k', 'spdz2k-party.x', '--ring 128'), # ok slow
 # ('lowgear', 'lowgear-party.x', ''), # ok? slow
 # ('highgear', 'highgear-party.x', ''), # ok? slow
 ]]
 
 dishonest_majority_covert = [t + ('DM-C-SS',) for t in [
-# ('cowgear', 'cowgear-party.x', ''), # ok
-# ('chaigear', 'chaigear-party.x', ''), # ok slow
+('cowgear', 'cowgear-party.x', ''), # ok
+('chaigear', 'chaigear-party.x', ''), # ok slow
 ]]
 
 dishonest_majority_semi_honest = [t + ('DM-SH-SS',) for t in [
@@ -20,7 +20,7 @@ dishonest_majority_semi_honest = [t + ('DM-SH-SS',) for t in [
 ]]
 
 honest_majority_malicious = [t + ('HM-M',) for t in [
-('ring', 'replicated-ring-party.x', '--ring 128'), # ok
+# ('ring', 'replicated-ring-party.x', '--ring 128'), # ok
 # ('ps-rep-ring', 'ps-rep-ring-party.x', '--ring 128'), # ok
 # ('mal-rep-ring', 'malicious-rep-ring-party.x', '--ring 128'), # ok
 # ('sy-rep-ring', 'sy-rep-ring-party.x', '--ring 128'), # ok
