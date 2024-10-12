e5b4699bea4c56e817ec983167042905c9a89801
exfinen <47593166+exfinen@users.noreply.github.com>
2024-10-13 04:43:45 +0900

    wip

--- mpcstats/benchmark/protocols.py
+++ mpcstats/benchmark/protocols.py
@@ -12,7 +12,7 @@ dishonest_majority_covert = [t + ('DM-C-SS',) for t in [
 ]]
 
 dishonest_majority_semi_honest = [t + ('DM-SH-SS',) for t in [
-('semi', 'semi-party.x', ''), # ok
+#('semi', 'semi-party.x', ''), # ok
 # ('semi2k', 'semi2k-party.x', '--ring 128'), # ok
 # ('hemi', 'hemi-party.x', ''), # ok
 # ('temi', 'temi-party.x', ''), # ok
@@ -20,12 +20,12 @@ dishonest_majority_semi_honest = [t + ('DM-SH-SS',) for t in [
 ]]
 
 honest_majority_malicious = [t + ('HM-M',) for t in [
-#('ring', 'replicated-ring-party.x', '--ring 128'), # ok
+('ring', 'replicated-ring-party.x', '--ring 128'), # ok
 #('ps-rep-ring', 'ps-rep-ring-party.x', '--ring 128'), # ok
 #('mal-rep-ring', 'malicious-rep-ring-party.x', '--ring 128'), # ok
 #('sy-rep-ring', 'sy-rep-ring-party.x', '--ring 128'), # ok
 #('rep4-ring', 'rep4-ring-party.x', '--ring 128'), # ok, ssl, 4 parties
-# ('ps-rep-field', 'ps-rep-field-party.x', ''), # ok
+('ps-rep-field', 'ps-rep-field-party.x', ''), # ok
 # ('sy-rep-field', 'sy-rep-field-party.x', ''), # ok
 # ('mal-rep-field', 'malicious-rep-field-party.x', ''), # ok
 # ('mal-shamir', 'malicious-shamir-party.x', ''), # ok
