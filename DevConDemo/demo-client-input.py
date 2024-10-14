87a7c6522251cd63eb3f21f6cf31c613386249d4
JernKunpittaya <61564542+JernKunpittaya@users.noreply.github.com>
2024-10-14 23:48:18 +0700

    remove reverse_bytes()

--- DevConDemo/demo-client-input.py
+++ DevConDemo/demo-client-input.py
@@ -19,10 +19,22 @@ for socket in client.sockets:
     os.store(0)
     os.Send(socket)
 
+def reverse_bytes(integer):
+    # Convert integer to bytes, assuming it is a 32-bit integer (4 bytes)
+    byte_length = (integer.bit_length() + 7) // 8 or 1
+    byte_representation = integer.to_bytes(byte_length, byteorder='big')
+    
+    # Reverse the byte order
+    reversed_bytes = byte_representation[::-1]
+    
+    # Convert the reversed bytes back to an integer
+    reversed_integer = int.from_bytes(reversed_bytes, byteorder='big')
+    
+    return reversed_integer
+
 def run(x):
-    client.send_private_inputs([x, 19025386729892471294905774323873829730555352566281815376957579075809073893907])
-    # client.send_private_inputs([x,3030918970555839371,6351928071260488773, 16545997817213844656, 3833218938854117907 ])
-    #client.send_private_inputs([x])
+    nonce = 19025386729892471294905774323873829730555352566281815376957579075809073893907
+    client.send_private_inputs([x, reverse_bytes(nonce)])
     print("finish sending private inputs")
     # print('Winning client id is :', client.receive_outputs(1)[0])
 
--- DevConDemo/demo-client-output.py
+++ DevConDemo/demo-client-output.py
@@ -20,13 +20,25 @@ for socket in client.sockets:
     # computationIndex is public, not need to be secret shared.
     os.store(computationIndex)
     os.Send(socket)
+    
+def reverse_bytes(integer):
+    # Convert integer to bytes, assuming it is a 32-bit integer (4 bytes)
+    byte_length = (integer.bit_length() + 7) // 8 or 1
+    byte_representation = integer.to_bytes(byte_length, byteorder='big')
+    
+    # Reverse the byte order
+    reversed_bytes = byte_representation[::-1]
+    
+    # Convert the reversed bytes back to an integer
+    reversed_integer = int.from_bytes(reversed_bytes, byteorder='big')
+    
+    return reversed_integer
 
 def run():
     output_list = client.receive_outputs(1+MAX_NUM_CLIENTS)
     print('Computation index',computationIndex, "is", output_list[0])
     for i in range(MAX_NUM_CLIENTS):
-        print('commitment: ',i, 'is', output_list[i+1])
-    # print('commitment index is', client.receive_outputs(2)[1])
+        print('commitment: ',i, 'is', hex(reverse_bytes(output_list[i+1])))
 
 # running two rounds
 # first for sint, then for sfix
