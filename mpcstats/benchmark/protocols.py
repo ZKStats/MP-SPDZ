224b60b5a9945cb5ec009425849c02955f67f353
exfinen <47593166+exfinen@users.noreply.github.com>
2024-10-16 13:22:11 +0900

    wip

--- mpcstats/benchmark/protocols.py
+++ mpcstats/benchmark/protocols.py
@@ -1,7 +1,7 @@
 dishonest_majority_malicious = [t + ('DM-M-SS',) for t in [
-('mascot', 'mascot-party.x', ''), # ok
-('mama', 'mama-party.x', ''), # ok
-('spdz2k', 'spdz2k-party.x', '--ring 128'), # ok slow
+# ('mascot', 'mascot-party.x', ''), # ok
+# ('mama', 'mama-party.x', ''), # ok
+# ('spdz2k', 'spdz2k-party.x', '--ring 128'), # ok slow
 # ('lowgear', 'lowgear-party.x', ''), # ok? slow
 # ('highgear', 'highgear-party.x', ''), # ok? slow
 ]]
